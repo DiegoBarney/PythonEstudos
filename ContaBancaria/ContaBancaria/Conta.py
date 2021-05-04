ff74a446c074924f7af8e36329965f57f9ed75c9
DiegoBarney <komyhu@hotmail.com>
2021-05-04 20:48:16 -0300

    aula 5
    propriedades e getter / setter

--- ContaBancaria/ContaBancaria/Conta.py
+++ ContaBancaria/ContaBancaria/Conta.py
@@ -1,18 +1,18 @@
 
 class Conta:
 
-    def __init__(self, conta, agencia, saldo, titular):
+    def __init__(self, conta, agencia, saldo, titular, limite = 200):
         self.__conta = conta
         self.__agencia = agencia
         self.__saldo = saldo
         self.__titular = titular
-
+        self.__limite = limite
+   
 
     def sacar(self, valor):
         if valor < self.__saldo:
             self.__saldo -= valor
   
-
     def depositar(self, valor):
         if valor > 0:
             self.__saldo += valor
@@ -23,6 +23,27 @@ class Conta:
             contaDestino.depositar(valor)
 
     def extrato_conta(self):
-        print("Titular = {0}, Conta = {1}, Agencia = {2}, Saldo = {3}".format(self.__titular, self.__conta, self.__agencia, self.__saldo))
+        print("Titular = {0}, Conta = {1}, Agencia = {2}, Saldo = {3}, limite = {4}".format(self.__titular, self.__conta, self.__agencia, self.__saldo, self.__limite))
 
-   
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_agencia(self):
+        return self.__agencia
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @titular.setter
+    def titular(self, titular):
+       self.__titular = titular.title()
+
+    def get_conta(self):
+        return self.__conta
+
+    def get_limite(self):
+        return self.__limite
+
+    def set_limite(self, novoLimite):
+        self.__limite = novoLimite
--- ContaBancaria/ContaBancaria/ContaBancaria.py
+++ ContaBancaria/ContaBancaria/ContaBancaria.py
@@ -1,11 +1,15 @@
 import Conta
 
 
-cont = Conta.Conta(113,"30226-2",120.0,"Diego Pimentel")
-cont2 = Conta.Conta(111,"1111-3",150.0,"Joao")
+cont = Conta.Conta(113,"30226-2",120.0,"diego Pimentel")
+cont2 = Conta.Conta(111,"1111-3",150.0,"joao")
 cont._Conta__conta = 1
 
-cont.transferir(cont2,50)
 
+cont.transferir(cont2,50)
+#cont.titular = "joao"
+print(cont.titular)
 cont.extrato_conta()
 cont2.extrato_conta()
+
+
