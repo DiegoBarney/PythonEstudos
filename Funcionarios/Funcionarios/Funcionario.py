1382dd700d15c19a6730f9230cd18f95bcde1338
DiegoBarney <komyhu@hotmail.com>
2021-05-11 21:42:25 -0300

    Aula 9
    DateTime Python

--- Funcionarios/Funcionarios/Funcionario.py
+++ Funcionarios/Funcionarios/Funcionario.py
@@ -1,12 +1,30 @@
+from datetime import datetime
+
 class Funcionario:
     def __init__(self, nome):
         self.nome = nome
+        self.__marcacao_saida = 0
+        self.__marcacao_entrada = 0
 
-    def registra_horas(self, horas):
-        print('Horas registradas...')
+    def registra_horas_entrada(self):
+        self.__marcacao_entrada = datetime.now()
+
+    def registra_horas_saida(self):
+        if self.__marcacao_entrada != datetime.now():
+            self.__marcacao_saida = datetime.now()
+        else:
+            raise Exception("Não pode marcar dois pontos seguidos")
 
     def mostrar_tarefas(self):
         print('Fez muita coisa...')
 
+
+    @property
+    def marcacao_entrada(self):
+        return self.__marcacao_entrada.strftime("%d/%m/%Y %H:%M:%S")
+
+    @property
+    def marcacao_saida(self):
+        return self.__marcacao_saida
 
 
--- Funcionarios/Funcionarios/Funcionarios.py
+++ Funcionarios/Funcionarios/Funcionarios.py
@@ -4,3 +4,6 @@ Diego = Senior.Senior("Diego")
 print(Diego)
 Diego.busca_perguntas_sem_resposta()
 Diego.mostrar_tarefas()
+
+Diego.registra_horas_entrada()
+print(Diego.marcacao_entrada)
